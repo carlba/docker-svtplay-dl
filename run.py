564c6893a1b2d7203f14f920d39fc4dd9ce10e53
Carl Bäckström <genzorg@gmail.com>
2019-06-03 22:07:20 +0200

    It is now possible to download both episodes and seasons
     -  Using the SVTPLAY_DL_SEASON_URLS and SVTPLAY_DL_EPISODE_URLS
        respectively

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import sys
 import time
+from typing import Optional
 
 import click
 from click.testing import CliRunner
@@ -12,29 +13,48 @@ def is_debugging():
     return not (sys.gettrace() is None)
 
 
-def monitor(svtplay_dl_outpath: str, svtplay_dl_urls: []):
+def monitor(svtplay_dl_outpath: str, svtplay_dl_season_urls: [] = None,
+            svtplay_dl_episode_urls: [] = None):
 
     while True:
-        click.echo(f'Trying to download these URLs {svtplay_dl_urls}')
         try:
-            svtplay_dl('--output', svtplay_dl_outpath, '-A', '--remux', *svtplay_dl_urls, _fg=True)
+            if svtplay_dl_season_urls:
+                click.echo(f'Trying to download these season URLs {svtplay_dl_season_urls}')
+                svtplay_dl('--output', svtplay_dl_outpath, '-A', '--remux',
+                           *svtplay_dl_season_urls, _fg=True)
+
+            if svtplay_dl_episode_urls:
+                click.echo(f'Trying to download these episode URLs {svtplay_dl_episode_urls}')
+                svtplay_dl('--output', svtplay_dl_outpath, '--remux',
+                           *svtplay_dl_episode_urls, _fg=True)
         except sh.ErrorReturnCode as err:
             click.echo(err)
             click.echo(err.stderr)
-
         finally:
             click.echo(f'Finished downloading trying again in {60*30} seconds')
             time.sleep(60*30)
 
 
 @click.command()
-@click.argument('svtplay_dl_outpath', envvar='SVTPLAY_DL_OUTPATH')
-@click.argument('svtplay_dl_urls', envvar='SVTPLAY_DL_URLS')
-def main(svtplay_dl_outpath: str, svtplay_dl_urls: str):
+@click.argument('svtplay_dl_outpath', envvar='SVTPLAY_DL_OUTPATH', required=False,
+                default=None)
+@click.argument('svtplay_dl_season_urls', envvar='SVTPLAY_DL_SEASON_URLS', required=False,
+                default=None)
+@click.argument('svtplay_dl_episode_urls', envvar='SVTPLAY_DL_EPISODE_URLS')
+def main(svtplay_dl_outpath: str, svtplay_dl_season_urls: Optional[str],
+         svtplay_dl_episode_urls: Optional[str]):
     """Console script for docker-svtplay-dl"""
 
-    svtplay_dl_urls_list = [x.strip() for x in svtplay_dl_urls.split(',')] or [svtplay_dl_urls]
-    monitor(svtplay_dl_outpath, svtplay_dl_urls_list)
+    dl_season_list, dl_episode_list = None, None
+
+    if svtplay_dl_season_urls:
+        dl_season_list = [x.strip()
+                          for x in svtplay_dl_season_urls.split(',')] or [svtplay_dl_season_urls]
+    if svtplay_dl_episode_urls:
+        dl_episode_list = [x.strip()
+                           for x in svtplay_dl_episode_urls.split(',')] or [svtplay_dl_episode_urls]
+
+    monitor(svtplay_dl_outpath, dl_season_list, dl_episode_list)
 
 
 if __name__ == "__main__":
