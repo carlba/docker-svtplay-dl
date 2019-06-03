2f5b4ff487598dcd885dae37b9f988f4a1aaa83b
Carl Bäckström <genzorg@gmail.com>
2019-06-03 16:22:21 +0200

    The download is only executed every 60*30 seconds

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 import click
 from click.testing import CliRunner
@@ -11,6 +12,22 @@ def is_debugging():
     return not (sys.gettrace() is None)
 
 
+def monitor(svtplay_dl_outpath: str, svtplay_dl_urls: []):
+
+    while True:
+        click.echo(f'Trying to download these URLs {svtplay_dl_urls}')
+        try:
+            svtplay_dl('--output', svtplay_dl_outpath, '--remux', *svtplay_dl_urls, _fg=True)
+        except sh.ErrorReturnCode as err:
+            click.echo(err)
+            click.echo(err.stderr)
+
+        finally:
+            click.echo(f'Finished downloading trying again in {60*30} seconds')
+
+        time.sleep(60*30)
+
+
 @click.command()
 @click.argument('svtplay_dl_outpath', envvar='SVTPLAY_DL_OUTPATH')
 @click.argument('svtplay_dl_urls', envvar='SVTPLAY_DL_URLS')
@@ -18,13 +35,7 @@ def main(svtplay_dl_outpath: str, svtplay_dl_urls: str):
     """Console script for docker-svtplay-dl"""
 
     svtplay_dl_urls_list = [x.strip() for x in svtplay_dl_urls.split(',')] or [svtplay_dl_urls]
-    print (" ".join(svtplay_dl_urls_list))
-
-    try:
-        svtplay_dl('--output', svtplay_dl_outpath, '--remux', *svtplay_dl_urls_list, _fg=True)
-    except sh.ErrorReturnCode as err:
-        print(err)
-        print(err.stderr)
+    monitor(svtplay_dl_outpath, svtplay_dl_urls_list)
 
 
 if __name__ == "__main__":
